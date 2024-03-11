bdbf5d381e79db50a6af107b1e6be65cdfc7b608
BladiCreator <51289587+BladiCreator@users.noreply.github.com>
2024-03-11 14:26:31 +0100

    fix: arreglos en los atributos del dto de company

--- src/resources/companies/dto/create-company.dto.ts
+++ src/resources/companies/dto/create-company.dto.ts
@@ -1,17 +1,28 @@
 import { Application } from "@src/resources/applications/entities/application.entity";
+import { User } from "@src/resources/users/entities/user.entity";
 import { IsArray, IsNotEmpty, IsString, MaxLength } from "class-validator";
 
 export class CreateCompanyDto {
 	@IsNotEmpty()
 	@IsString()
 	@MaxLength(50)
-	name = "";
+	name: string;
 
 	@IsString()
 	@IsNotEmpty()
 	@MaxLength(1200)
-	description = "";
+	description: string;
 
 	@IsArray()
-	applications: Application[] = [];
+	applications: Application[];
+
+	@IsArray()
+	users: User[];
+
+	constructor(name: string, description: string, applications: Application[], users: User[]) {
+		this.name = name;
+		this.description = description;
+		this.applications = applications;
+		this.users = users;
+	}
 }
--- src/resources/companies/entities/company.entity.ts
+++ src/resources/companies/entities/company.entity.ts
@@ -8,6 +8,7 @@ import {
 import { v4 as uuidv4 } from "uuid";
 
 import { Application } from "@src/resources/applications/entities/application.entity";
+import { User } from "@src/resources/users/entities/user.entity";
 
 @Entity({name: "companies"})
 export class Company {
@@ -23,4 +24,10 @@ export class Company {
 	@ManyToMany(() => Application)
 	@JoinTable()
 	applications!: Application[];
+
+	//TODO: Hacer las relaciones entre el usuario que es developer y la compañía
+	// @OneToMany(() => Application, (application) => application.company)
+	// @ManyToMany(() => User)
+	// @JoinTable()
+	// applications!: Application[];
 }
